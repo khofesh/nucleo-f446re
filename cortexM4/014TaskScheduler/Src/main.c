83e5e0420355ca2e40903041f9eae9dc77d7c8b6
fahmi ahmad <fahmiahmad@tutanota.com>
2022-11-04 22:26:43 +0700

    stack pointer setup

--- cortexM4/014TaskScheduler/Src/main.c
+++ cortexM4/014TaskScheduler/Src/main.c
@@ -19,44 +19,42 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "main.h"
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-void task1_handler();
-void task2_handler();
-void task3_handler();
-void task4_handler();
+uint32_t psp_of_tasks[MAX_TASKS] = {
+		T1_STACK_START,
+		T2_STACK_START,
+		T3_STACK_START,
+		T4_STACK_START,
+};
 
-void init_systick_timer(uint32_t tick_hz);
-__attribute__((naked)) void init_scheduler_stack(uint32_t sched_top_of_stack);
+uint32_t task_handlers[MAX_TASKS];
 
-void SysTick_Handler();
-
-// stack memory calculations
-#define SIZE_TASK_STACK 		1024U
-#define SIZE_SCHEDULER_STACK 	1024U
-
-// see STM32F446RETX_RAM.ld
-#define SRAM_START 				0x20000000U
-#define SIZE_RAM				(128 * 1024)
-#define SRAM_END				(SRAM_START + SIZE_RAM)
-
-#define T1_STACK_START			SRAM_END
-#define T2_STACK_START			(SRAM_END - (1 * SIZE_TASK_STACK))
-#define T3_STACK_START			(SRAM_END - (2 * SIZE_TASK_STACK))
-#define T4_STACK_START			(SRAM_END - (3 * SIZE_TASK_STACK))
-#define SCHED_STACK_START		(SRAM_END - (4 * SIZE_TASK_STACK))
-
-#define TICK_HZ 				1000U
-
-#define HSI_CLOCK				16000000U
-#define SYSTICK_TIMER_CLOCK		HSI_CLOCK
+uint8_t current_task = 0; // task1 is running
 
 int main(void)
 {
+	enable_processor_faults();
+
 	init_scheduler_stack(SCHED_STACK_START);
+
+	task_handlers[0] = (uint32_t)task1_handler;
+	task_handlers[1] = (uint32_t)task2_handler;
+	task_handlers[2] = (uint32_t)task3_handler;
+	task_handlers[3] = (uint32_t)task4_handler;
+
+	init_tasks_stack();
+
 	init_systick_timer(TICK_HZ);
+
+	switch_sp_to_psp();
+
+	task1_handler();
+
     /* Loop forever */
 	for(;;);
 }
@@ -121,6 +119,84 @@ __attribute__((naked)) void init_scheduler_stack(uint32_t sched_top_of_stack)
 {
 	__asm volatile("MSR MSP,%0"::"r"(sched_top_of_stack):);
 	__asm volatile("BX LR");
+}
+
+void init_tasks_stack()
+{
+	uint32_t *pPSP;
+
+	for(int i = 0; i < MAX_TASKS; i++)
+	{
+		pPSP = (uint32_t*) psp_of_tasks[i];
+
+		pPSP--;
+		*pPSP = DUMMY_XPSR; // 0x01000000
+
+		pPSP--; // PC
+		*pPSP = task_handlers[i];
+
+		pPSP--; // LR
+		*pPSP = 0xFFFFFFFD;
+
+		for (int j = 0; j < 13; j++)
+		{
+			pPSP--;
+			*pPSP = 0;
+		}
+
+		psp_of_tasks[i] = (uint32_t)pPSP;
+	}
+}
+
+uint8_t get_psp_value()
+{
+	return psp_of_tasks[current_task];
+}
+
+__attribute__((naked)) void switch_sp_to_psp()
+{
+	// initialize the PSP with task1 stack start
+
+	// get the value of psp of current_task
+	__asm volatile("PUSH {LR}"); // preserve LR which connect the main()
+	__asm volatile("BL get_psp_value");
+	__asm volatile("MSR PSP,R0"); // initialize PSP
+	__asm volatile("POP {LR}"); // pops back LR value
+
+	// change SP to PSP using CONTROL register
+	__asm volatile("MOV R0,#0X02");
+	__asm volatile("MSR CONTROL,R0");
+	__asm volatile("BX LR");
+}
+
+void enable_processor_faults()
+{
+	// enable all configurable exceptions
+	// usage fault, memManage fault and bus fault
+	uint32_t *pSHCSR = (uint32_t*)0xE000ED24;
+
+	// enable 18, 17, and 16 bit position
+	*pSHCSR |= (1 << 16); // MEMFAULTENA
+	*pSHCSR |= (1 << 17); // BUSFAULTENA
+	*pSHCSR |= (1 << 18); // USGFAULTENA
+}
+
+void HardFault_Handler()
+{
+	printf("exception: HardFault\n");
+	while(1);
+}
+
+void MemManage_Handler()
+{
+	printf("exception: MemManage\n");
+	while(1);
+}
+
+void BusFault_Handler()
+{
+	printf("exception: BusFault\n");
+	while(1);
 }
 
 void SysTick_Handler()
