2eb3922b1fe157022d59d91dca9f92c3c2b19285
fahmi ahmad <fahmiahmad@tutanota.com>
2022-12-07 22:15:03 +0700

    led toggling with push pull configuration

---
+++ masteringMCU/stm32f4xx_drivers/Src/001led_toggle.c
@@ -0,0 +1,39 @@
+/*
+ * 001led_toggle.c
+ *
+ *  Created on: Dec 7, 2022
+ *      Author: fahmad
+ */
+
+#include "stm32f446xx.h"
+#include "stm32f446xx_gpio_driver.h"
+
+void delay();
+
+int main(void)
+{
+	// toggle led on PA5
+	GPIO_Handle_t GpioLed;
+	GpioLed.pGPIOx = GPIOA;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUTPUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_Init(&GpioLed);
+
+	while(1)
+	{
+		GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+		delay();
+	}
+
+	return 0;
+}
+
+void delay()
+{
+	for (uint32_t i = 0; i < 500000/2; i++);
+}
