67c0fff960f35d1e288534bdbf2b23ca2fb4f3ae
fahmi ahmad <fahmiahmad@tutanota.com>
2022-12-22 23:48:29 +0700

    1. i2c specification 2. rcc register bit position 3. RCC_GetPCLK1Value

--- masteringMCU/stm32f4xx_drivers/drivers/Inc/stm32f446xx_i2c_driver.h
+++ masteringMCU/stm32f4xx_drivers/drivers/Inc/stm32f446xx_i2c_driver.h
@@ -8,6 +8,7 @@
 #ifndef INC_STM32F446XX_I2C_DRIVER_H_
 #define INC_STM32F446XX_I2C_DRIVER_H_
 
+#include <stdbool.h>
 #include "stm32f446xx.h"
 
 /**
--- masteringMCU/stm32f4xx_drivers/drivers/Src/stm32f446xx_i2c_driver.c
+++ masteringMCU/stm32f4xx_drivers/drivers/Src/stm32f446xx_i2c_driver.c
@@ -7,6 +7,12 @@
 
 #include "stm32f446xx_i2c_driver.h"
 
+uint32_t RCC_GetPCLK1Value();
+uint32_t RCC_GetPLLOutputClock();
+
+uint16_t AHB_PreScaler[8] = {2, 4, 8, 16, 64, 128, 256, 512};
+uint8_t APB1_PreScaler[4] = {2, 4, 8, 16};
+
 /**
  * @brief I2C_PeriClockControl
  *
@@ -56,6 +62,11 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi)
  */
 void I2C_Init(I2C_Handle_t *pI2CHandle)
 {
+    uint32_t tempReg = 0;
+
+    // ACK
+    tempReg |= pI2CHandle->I2C_Config.I2C_ACKControl << I2C_CR1_ACK;
+
     // 1. configure the mode (standard or fast)
 
     // 2. configure the speed of the serial clock (SCL)
@@ -136,6 +147,7 @@ void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi)
  */
 bool I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t flagName)
 {
+    return 1;
 }
 
 /**
@@ -146,4 +158,71 @@ bool I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t flagName)
  */
 void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle, uint8_t appEvent)
 {
+}
+
+uint32_t RCC_GetPCLK1Value()
+{
+    uint32_t pclk1;
+    uint32_t SystemClk;
+
+    uint8_t clksrc;
+    uint8_t temp;
+    uint8_t ahbp;
+    uint8_t apb1p;
+
+    /*
+        see RM0390-*.pdf page 133
+
+        Bits 3:2 SWS[1:0]: System clock switch status
+        Set and cleared by hardware to indicate which clock source is used as the system clock.
+        00: HSI oscillator used as the system clock
+        01: HSE oscillator used as the system clock
+        10: PLL used as the system clock
+        11: PLL_R used as the system clock
+    */
+    clksrc = ((RCC->CFGR >> RCC_CFGR_SWS_Pos) & 0x3);
+
+    if (clksrc == 0)
+    {
+        SystemClk = 16000000;
+    }
+    else if (clksrc == 1)
+    {
+        SystemClk = 8000000;
+    }
+    else if (clksrc == 2)
+    {
+        SystemClk = RCC_GetPLLOutputClock();
+    }
+
+    temp = ((RCC->CFGR >> RCC_CFGR_HPRE_Pos) & 0xFUL);
+
+    if (temp < 8)
+    {
+        ahbp = 1;
+    }
+    else
+    {
+        ahbp = AHB_PreScaler[temp - 8];
+    }
+
+    temp = ((RCC->CFGR >> RCC_CFGR_PPRE1_Pos) & 0x7UL);
+
+    if (temp < 4)
+    {
+        apb1p = 1;
+    }
+    else
+    {
+        apb1p = APB1_PreScaler[temp - 4];
+    }
+
+    pclk1 = (SystemClk / ahbp) / apb1p;
+
+    return pclk1;
+}
+
+uint32_t RCC_GetPLLOutputClock()
+{
+    return 0;
 }
