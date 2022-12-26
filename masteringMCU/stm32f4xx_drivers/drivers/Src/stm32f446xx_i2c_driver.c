322922bb9f4a1cbb6574a1fa3d0c6eabbe930f20
fahmi ahmad <fahmiahmad@tutanota.com>
2022-12-26 23:36:19 +0700

    slave logic

--- masteringMCU/stm32f4xx_drivers/drivers/Src/stm32f446xx_i2c_driver.c
+++ masteringMCU/stm32f4xx_drivers/drivers/Src/stm32f446xx_i2c_driver.c
@@ -663,6 +663,15 @@ void I2C_EV_IRQHandling(I2C_Handle_t *pI2CHandle)
                 I2C_MasterHandleTXEInterrupt(pI2CHandle);
             }
         }
+        else
+        {
+            /* slave */
+            // make sure that the slave is really in transmitter mode
+            if (pI2CHandle->pI2Cx->SR2 & (1 << I2C_SR2_TRA))
+            {
+                I2C_ApplicationEventCallback(pI2CHandle, I2C_EV_DATA_REQ);
+            }
+        }
     }
 
     temp3 = pI2CHandle->pI2Cx->SR1 & (1 << I2C_SR1_RXNE);
@@ -676,6 +685,15 @@ void I2C_EV_IRQHandling(I2C_Handle_t *pI2CHandle)
             if (pI2CHandle->TxRxState == I2C_BUSY_IN_RX)
             {
                 I2C_MasterHandleRXNEInterrupt(pI2CHandle);
+            }
+        }
+        else
+        {
+            /* slave */
+            // make sure that the slave is really in transmitter mode
+            if (pI2CHandle->pI2Cx->SR2 & (1 << I2C_SR2_TRA))
+            {
+                I2C_ApplicationEventCallback(pI2CHandle, I2C_EV_DATA_RCV);
             }
         }
     }
