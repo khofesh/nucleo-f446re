9aaf31d947caab143d66d779064be81461afa174
fahmi ahmad <fahmiahmad@tutanota.com>
2022-12-27 22:42:23 +0700

    systick handler

--- masteringMCU/stm32f4xx_drivers/Src/016rtc_lcd.c
+++ masteringMCU/stm32f4xx_drivers/Src/016rtc_lcd.c
@@ -9,10 +9,13 @@
 #include "stm32f446xx.h"
 #include "ds1307.h"
 
+#define SYSTICK_TIM_CLK 16000000UL
+
 char *get_day_of_week(uint8_t day);
 char *time_to_string(RTC_time_t *rtc_time);
 char *date_to_string(RTC_date_t *rtc_date);
 void number_to_string(uint8_t num, char *buf);
+void init_systick_timer(uint32_t tick_hz);
 
 int main()
 {
@@ -28,6 +31,8 @@ int main()
         }
     }
 
+    init_systick_timer(1);
+
     current_date.day = WEDNESDAY;
     current_date.date = 27;
     current_date.month = 12;
@@ -38,26 +43,30 @@ int main()
     current_time.seconds = 41;
     current_time.time_format = TIME_FORMAT_12HRS_PM;
 
-    ds1307_get_current_date(&current_date);
-    ds1307_get_current_time(&current_time);
+    ds1307_set_current_date(&current_date);
+    ds1307_set_current_time(&current_time);
 
     ds1307_get_current_time(&current_time);
     ds1307_get_current_date(&current_date);
 
     char *am_pm;
+    char *timeInString;
     if (current_time.time_format != TIME_FORMAT_24HRS)
     {
         am_pm = (current_time.time_format) ? "PM" : "AM";
-        printf("current time = %s %s\n", time_to_string(&current_time), am_pm);
+        timeInString = time_to_string(&current_time);
+        printf("current time = %s %s\n", timeInString, am_pm);
     }
     else
     {
-        printf("current time = %s\n", time_to_string(&current_time));
+        timeInString = time_to_string(&current_time);
+        printf("current time = %s\n", timeInString);
     }
 
     // 27/12/22 <wednesday>
-    printf("current date = %s <%s>\n", date_to_string(&current_date),
-           get_day_of_week(current_date.day));
+    char *dateInString = date_to_string(&current_date);
+    char *dayOfWeek = get_day_of_week(current_date.day);
+    printf("current date = %s <%s>\n", dateInString, dayOfWeek);
 
     while (1)
     {
@@ -141,4 +150,55 @@ void number_to_string(uint8_t num, char *buf)
         buf[0] = (num / 10) + 48;
         buf[1] = (num % 10) + 48;
     }
+}
+
+void init_systick_timer(uint32_t tick_hz)
+{
+    uint32_t *pSRVR = (uint32_t *)0xE000E014;
+    uint32_t *pSCSR = (uint32_t *)0xE000E010;
+
+    /* calculation of reload value */
+    uint32_t count_value = (SYSTICK_TIM_CLK / tick_hz) - 1;
+
+    // Clear the value of SVR
+    *pSRVR &= ~(0x00FFFFFFFF);
+
+    // load the value in to SVR
+    *pSRVR |= count_value;
+
+    // do some settings
+    *pSCSR |= (1 << 1); // Enables SysTick exception request:
+    *pSCSR |= (1 << 2); // Indicates the clock source, processor clock source
+
+    // enable the systick
+    *pSCSR |= (1 << 0); // enables the counter
+}
+
+void SysTick_Handler()
+{
+    RTC_time_t current_time;
+    RTC_date_t current_date;
+
+    ds1307_get_current_time(&current_time);
+
+    char *am_pm;
+    char *timeInString;
+    if (current_time.time_format != TIME_FORMAT_24HRS)
+    {
+        am_pm = (current_time.time_format) ? "PM" : "AM";
+        timeInString = time_to_string(&current_time);
+        printf("current time = %s %s\n", timeInString, am_pm);
+    }
+    else
+    {
+        timeInString = time_to_string(&current_time);
+        printf("current time = %s\n", timeInString);
+    }
+
+    ds1307_get_current_date(&current_date);
+
+    // 27/12/22 <wednesday>
+    char *dateInString = date_to_string(&current_date);
+    char *dayOfWeek = get_day_of_week(current_date.day);
+    printf("current date = %s <%s>\n", dateInString, dayOfWeek);
 }
