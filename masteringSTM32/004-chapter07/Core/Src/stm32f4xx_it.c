5d7c439701348efcbbddeb6ab0e29cab73baa879
fahmi ahmad <fahmiahmad@tutanota.com>
2023-01-20 22:43:37 +0700

    priority

--- masteringSTM32/004-chapter07/Core/Src/stm32f4xx_it.c
+++ masteringSTM32/004-chapter07/Core/Src/stm32f4xx_it.c
@@ -204,9 +204,10 @@ void SysTick_Handler(void)
 void EXTI15_10_IRQHandler(void)
 {
   /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
 
   /* USER CODE END EXTI15_10_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(B1_Pin);
+//  HAL_GPIO_EXTI_IRQHandler(B1_Pin);
   /* USER CODE BEGIN EXTI15_10_IRQn 1 */
 
   /* USER CODE END EXTI15_10_IRQn 1 */
